ad8406072399794d1e7f7c29baffdfec588b09ac
BangUl-Pro <wind.dong.dream@gmail.com>
2016-12-31 01:34:43 +0900

    픽셀 연산 구현

--- IppImage/IppEnhance.cpp
+++ IppImage/IppEnhance.cpp
@@ -117,30 +117,133 @@ void IppHistogramEqualization(IppByteImage& img) {
 
 
 bool IppAdd(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3) {
-	return TRUE;
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = limit(p1[i] + p2[i]);
+	}
+
+	return true;
 }
 
 
 bool IppSub(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3) {
-	return TRUE;
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = limit(p1[i] - p2[i]);
+	}
+	return true;
 }
 
 
 bool IppAve(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3) {
-	return TRUE;
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = (p1[i] + p2[i]) / 2;
+	}
+	return true;
 }
 
 
 bool IppDiff(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3) {
-	return TRUE;
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = (p1[i] > p2[i] ? p1[i] - p2[i] : p2[i] - p1[i]);
+	}
+	return true;
 }
 
 
 bool IppAND(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3) {
-	return TRUE;
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = p1[i] & p2[i];
+	}
+	return true;
 }
 
 
 bool IppOR(IppByteImage& img1, IppByteImage& img2, IppByteImage& img3) {
-	return TRUE;
+	int w = img1.GetWidth();
+	int h = img1.GetHeight();
+
+	if (w != img2.GetWidth() || h != img2.GetHeight())
+		return false;
+
+	img3.CreateImage(w, h);
+
+	int size = img3.GetSize();
+	BYTE* p1 = img1.GetPixels();
+	BYTE* p2 = img2.GetPixels();
+	BYTE* p3 = img3.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p3[i] = p1[i] | p2[i];
+	}
+	return true;
 }
